b9f49af81b66a790a1afeba28194e97b383e9955
rae-glazier <glazier.home.life@gmail.com>
2019-12-06 07:17:51 -0600

    basic structure/code completed..now onwards to testing

--- my-app/src/components/dashboard.js
+++ my-app/src/components/dashboard.js
@@ -1,5 +1,50 @@
-
-
+import React from 'react';
 
 // button for : strike, ball, foul, hit
 /// changes shouold update info in display
+
+// balls/strikes rest to 0 when reaches 3 strikes or 4 balls
+// balls/strikes reset to 0 when hit is recorded
+// foul increases strikes up to 2
+// no strikes ~ a foul makes it 1 strike
+// 1 strike ~ a foul makes it 2 strikes 
+// 2 strikes ~ a foul has no effect
+// display the count of balls/strikes for the at-bat
+// updates when usser records activity on the dashboard component
+
+ const Dashboard = (props) => {
+
+    return(
+          <section className='buttons'>
+
+            <div className='strike-button'>
+                <button onClick={props.handleStrike}>
+                    Strike
+                </button>
+            </div>
+
+            <div className='ball-button'>
+                <button onClick={props.handleBalls}>
+                    Ball
+                </button>
+            </div>
+
+            <div className='foul-button'>
+                <button onClick={props.handleFoul}>
+                    Foul
+                </button>
+            </div>
+
+            <div className='hit-button'>
+                <button onClick={props.handleHit}>
+                    Hit
+                </button>
+            </div>
+
+    </section>
+    )
+  
+
+}
+
+export default Dashboard;
--- my-app/src/components/display.js
+++ my-app/src/components/display.js
@@ -1,11 +1,15 @@
+import React from 'react';
 
 
+const Display = (props) => {
+    
+    return(
 
-// balls/strikes rest to 0 when reaches 3 strikes or 4 balls
-// balls/strikes reset to 0 when hit is recorded
-// foul increases strikes up to 2
-// no strikes ~ a foul makes it 1 strike
-// 1 strike ~ a foul makes it 2 strikes 
-// 2 strikes ~ a foul has no effect
-// display the count of balls/strikes for the at-bat
-// updates when usser records activity on the dashboard component
+        <div>
+            <h2>Balls: {props.balls}</h2>
+            <h2>Strikes: {props.strike}</h2>
+        </div>
+    )
+}
+
+export default Display;
